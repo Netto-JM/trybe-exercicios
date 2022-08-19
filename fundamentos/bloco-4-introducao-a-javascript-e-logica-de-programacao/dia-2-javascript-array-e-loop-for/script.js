5da3d800fc310bd3829996ba755905aca5402436
NettoJM <nettojm86@gmail.com>
2022-08-19 19:19:31 -0300

    calculando a media e imprimindo mensagem se a media e maior ou menor que 20

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,6 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let sum = 0;
 for (let index = 0; index < numbers.length; index++) {
   const number = numbers[index];
   console.log(number);
+  sum += numbers[index];
+}
+console.log(sum);
+const average = sum/numbers.length
+console.log(average);
+if (average > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor que 20');
 }
