cea4dab6e361eea5338242c334d27c477e5ae467
NettoJM <nettojm86@gmail.com>
2022-08-19 00:05:59 -0300

    funcao isTriangle adicionada

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -29,3 +29,26 @@ function maxDeTres(a, b, c) {
 function isTrue(bool) {
   return bool === true ? "positive" : bool === false ? "negative" : "zero";
 }
+
+function isTriangle(a, b, c) {
+  let error = '';
+  function evaluateAngles(acc, curr, i, arr) {
+    // debugging start
+    console.log(acc);
+    console.log(curr);
+    console.log((typeof curr !== 'number'));
+    console.log(isNaN(curr));
+    console.log(curr <= 0);
+    // debugging end
+    error = (typeof curr !== 'number') || isNaN(curr) ? 'Por favor digite um número válido' 
+    : curr <= 0 ? 'Por favor digite um número positivo' : '';
+    error && arr.splice(1) // eject early
+    return acc + curr;
+  }
+  const sumAngles = [a, b, c].reduce(evaluateAngles, 1) - 1;
+  const result = sumAngles === 180 ? true : false;
+  console.log(error);
+  return error || result;
+}
+
+console.log(isTriangle(12, 2, 166));
