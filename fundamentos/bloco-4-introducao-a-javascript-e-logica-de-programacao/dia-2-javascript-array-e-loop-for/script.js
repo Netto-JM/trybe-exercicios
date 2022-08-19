21e3d4aafbb90fb42009e575ceae173e2be21c5e
NettoJM <nettojm86@gmail.com>
2022-08-19 19:24:27 -0300

    valor maior e menor calculado e exibido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let max = numbers[0];
+let min = numbers[0];
 for (let index = 0; index < numbers.length; index++) {
   const number = numbers[index];
   console.log(number);
   sum += numbers[index];
+  max = numbers[index] > max ? numbers[index] : max;
+  min = numbers[index] < min ? numbers[index] : min;
 }
 console.log(sum);
 const average = sum/numbers.length
@@ -13,3 +17,5 @@ if (average > 20) {
 } else {
   console.log('valor menor que 20');
 }
+console.log(max);
+console.log(min);
