74793eb24e15e5df15e9f778d5c9fa521299ef8a
NettoJM <nettojm86@gmail.com>
2022-08-18 21:41:15 -0300

    primeiras quatro funcoes

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,20 @@
+function adicao(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
