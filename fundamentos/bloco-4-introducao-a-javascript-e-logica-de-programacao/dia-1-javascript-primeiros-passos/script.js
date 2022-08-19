274df3a4af80f5c3cce3177d579110737196c539
NettoJM <nettojm86@gmail.com>
2022-08-19 00:07:59 -0300

    removido comentário e console.log

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -33,22 +33,12 @@ function isTrue(bool) {
 function isTriangle(a, b, c) {
   let error = '';
   function evaluateAngles(acc, curr, i, arr) {
-    // debugging start
-    console.log(acc);
-    console.log(curr);
-    console.log((typeof curr !== 'number'));
-    console.log(isNaN(curr));
-    console.log(curr <= 0);
-    // debugging end
     error = (typeof curr !== 'number') || isNaN(curr) ? 'Por favor digite um número válido' 
     : curr <= 0 ? 'Por favor digite um número positivo' : '';
-    error && arr.splice(1) // eject early
+    error && arr.splice(1) // encerra mais cedo
     return acc + curr;
   }
   const sumAngles = [a, b, c].reduce(evaluateAngles, 1) - 1;
   const result = sumAngles === 180 ? true : false;
-  console.log(error);
   return error || result;
 }
-
-console.log(isTriangle(12, 2, 166));
