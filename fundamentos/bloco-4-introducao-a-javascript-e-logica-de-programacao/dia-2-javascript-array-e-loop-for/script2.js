49b4aab7da414244a87e5bc28507ef720cd0fea9
NettoJM <nettojm86@gmail.com>
2022-08-20 00:48:52 -0300

    array com valores multiplicados pelo proximo elemento criado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script2.js
@@ -1,6 +1,7 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 const ascendingNumbers = [...numbers]
 const descendingNumbers = [...numbers]
+const numbersMultiplied = [];
 
 for (let index = 1; index < ascendingNumbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
@@ -22,5 +23,10 @@ for (let index = 1; index < descendingNumbers.length; index += 1) {
   }
 }
 
+for (let index = 0; index < numbers.length; index += 1) {
+  numbersMultiplied.push(numbers[index] * (numbers[index+1] || 2))
+}
+
 console.log(ascendingNumbers);
 console.log(descendingNumbers);
+console.log(numbersMultiplied);
