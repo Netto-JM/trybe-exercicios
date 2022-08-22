2b8fe75bb8c4a311c00d3902df7393122e6f163a
NettoJM <nettojm86@gmail.com>
2022-08-21 23:53:33 -0300

    reverseWordPracticalWay implementada

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -8,3 +8,12 @@ function factorial(number) {
   if (number <= 1) return 1;
   return number * factorial(number - 1);
 }
+
+console.log(factorial(10));
+
+function reverseWordPracticalWay(word) {
+  const reversedWord = word.split('').reverse().join('');
+  return reversedWord;
+}
+
+console.log(reverseWordPracticalWay('banana'));
