b11e748c5a82020267330ec5bfd5901aa29b1a8c
NettoJM <nettojm86@gmail.com>
2022-08-22 18:47:55 -0300

    funcao drawRectangle finalizada

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script2.js
@@ -0,0 +1,27 @@
+function drawRectangle(number) {
+  for (let i = 0; i < number; i++) {
+    let row = ''
+    for (let j = 0; j < number; j++) {
+      row += '*'
+    }
+    console.log(row);
+  }
+}
+
+drawRectangle(5)
+
+function drawTriangleRectangleLeft(number) {
+  //code here
+}
+
+function drawTriangleRectangleRight(number) {
+  //code here
+}
+
+function drawPiramid(number) {
+  //code here
+}
+
+function drawHollowPiramid(number) {
+  //code here
+}
