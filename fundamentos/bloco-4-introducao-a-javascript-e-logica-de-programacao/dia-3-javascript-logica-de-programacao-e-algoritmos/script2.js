1fa1ba674c36c426fe0e84049fd0548d25fe30ff
NettoJM <nettojm86@gmail.com>
2022-08-22 22:22:53 -0300

    funcao drawPiramid finalizada com helper functions drawPiramidAsterisks e drawSpacesAroundAsterisks

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script2.js
@@ -11,12 +11,44 @@ function drawRectangle(number) {
 drawRectangle(5)
 
 
+function drawPiramidAsterisks(index, stopper) {
+  let row = '';
+  for (; index >= stopper; index -= 1) {
+    row += '*';
+  }
+  return row;
+}
+
+function drawSpacesAroundAsterisks(row, base) {
+  const numberOfSpaces = base - row.length;
+  for (let index = 0; index < numberOfSpaces; index += 1) {
+    if (index % 2 === 0) row += ' ';
+    else row = ' ' + row;
+  }
+  return row
+}
+
+function isPiramidTop(row) {
+  if (row.indexOf('*') === -1) return 'Invalid row';
+  return row.indexOf('*') === row.lastIndexOf('*')
+}
+
+function drawPiramid(number) {
+  for (let outerIndex = 0; outerIndex < number; outerIndex += 2) {
+    let row = drawPiramidAsterisks(number, number - outerIndex);
+    row = drawSpacesAroundAsterisks(row, number)
+    console.log(outerIndex);
+    console.log(row);
+    console.log(row.length);
+  }
+}
+
+drawPiramid(5)
+
+
 function drawTriangleRectangleLeft(number) {
   for (let outerIndex = 0; outerIndex < number; outerIndex += 1) {
-    let row = ''
-    for (let innerIndex = number; innerIndex >= number - outerIndex; innerIndex -= 1) {
-      row += '*'
-    }
+    let row = drawPiramidAsterisks(number, number - outerIndex);
     console.log(row);
   }
 }
@@ -43,11 +75,6 @@ function drawTriangleRectangleRight(number) {
 }
 
 drawTriangleRectangleRight(5)
-
-
-function drawPiramid(number) {
-  //code here
-}
 
 
 function drawHollowPiramid(number) {
