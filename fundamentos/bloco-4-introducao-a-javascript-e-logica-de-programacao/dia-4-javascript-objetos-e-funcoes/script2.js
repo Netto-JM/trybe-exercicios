126d33dd2bf7040d0b20614037e97a43021fe2fb
NettoJM <nettojm86@gmail.com>
2022-08-24 03:06:24 -0300

    terceira function criada, segunda corrigida

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -3,8 +3,18 @@ function isPalindrome(word) {
   return (word === reversedWord)
 }
 
-function findMaxNumber(arrayOfNumbers) {
-  return Math.max(...arrayOfNumbers)
+function findMaxNumberIndex(arrayOfNumbers) {
+  const maxNumber = Math.max(...arrayOfNumbers);
+  const maxNumberIndex = arrayOfNumbers.indexOf(maxNumber);
+  return maxNumberIndex;
 }
 
-console.log(findMaxNumber([6,2,4,7,1]));
+console.log(findMaxNumberIndex([6,2,4,7,3]));
+
+function findMinNumberIndex(arrayOfNumbers) {
+  const minNumber = Math.min(...arrayOfNumbers);
+  const minNumberIndex = arrayOfNumbers.indexOf(minNumber);
+  return minNumberIndex;
+}
+
+console.log(findMinNumberIndex([6, 2, 4, 7, 3]));
