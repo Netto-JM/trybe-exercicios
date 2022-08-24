8aa01136a6bbe2cb138fd3cdd48b61dbb2d1896b
NettoJM <nettojm86@gmail.com>
2022-08-24 03:16:05 -0300

    findLongestWord criada

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -1,7 +1,12 @@
 function isPalindrome(word) {
-  let reversedWord = word.split('').reverse().join('');
+  const reversedWord = word.split('').reverse().join('');
   return (word === reversedWord)
 }
+
+console.log(isPalindrome('arara'));
+
+console.log(isPalindrome('carro'));
+
 
 function findMaxNumberIndex(arrayOfNumbers) {
   const maxNumber = Math.max(...arrayOfNumbers);
@@ -11,10 +16,23 @@ function findMaxNumberIndex(arrayOfNumbers) {
 
 console.log(findMaxNumberIndex([6,2,4,7,3]));
 
-function findMinNumberIndex(arrayOfNumbers) {
-  const minNumber = Math.min(...arrayOfNumbers);
-  const minNumberIndex = arrayOfNumbers.indexOf(minNumber);
+function findMinNumberIndex(numbersArray) {
+  const minNumber = Math.min(...numbersArray);
+  const minNumberIndex = numbersArray.indexOf(minNumber);
   return minNumberIndex;
 }
 
 console.log(findMinNumberIndex([6, 2, 4, 7, 3]));
+
+
+function findLongestWord(wordsArray) {
+  let longestWord = '';
+  for (let index = 0; index < wordsArray.length; index++) {
+    if (wordsArray[index].length > longestWord.length) {
+      longestWord = wordsArray[index];
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
