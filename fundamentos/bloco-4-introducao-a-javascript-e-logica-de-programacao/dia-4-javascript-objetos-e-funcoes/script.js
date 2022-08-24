cdd3e1c5cdfa8c2da507305f73a6c85e2ad3550c
NettoJM <nettojm86@gmail.com>
2022-08-24 00:18:19 -0300

    ultimos tres exercícios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -26,7 +26,30 @@ const info2 = {
 for (const key in info) {
   if (info[key] === info2[key]) {
     console.log('Ambos recorrentes');
-    continue;
+    break;
   }
   console.log(`${info[key]} e ${info2[key]}`);
 }
+
+
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [{
+    titulo: 'O Pior Dia de Todos',
+    autor: 'Daniela Kopsch',
+    editora: 'Tordesilhas',
+  }, ],
+};
+
+console.log(`O livro favorito de ${leitor['nome']} ${leitor['sobrenome']} se chama '${leitor['livrosFavoritos'][0]['titulo']}'`);
+
+leitor['livrosFavoritos'].push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+
+console.log(`${leitor['nome']} tem ${leitor['livrosFavoritos'].length} livros favoritos`);
