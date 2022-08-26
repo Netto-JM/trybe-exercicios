9dbe5419dfaa6f69618f8afdb635360d7b76b66e
NettoJM <nettojm86@gmail.com>
2022-08-26 13:15:33 -0300

    code reorganized

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script3.js
@@ -3,7 +3,6 @@ const vector = [
   [3, 4, 5, 6],
   [7, 8, 9, 10]
 ];
-
 function arrayOfNumbers(vector) {
   const evenNumbers = [];
   const flatenedVector = vector.flat();
@@ -15,7 +14,72 @@ function arrayOfNumbers(vector) {
   }
   return evenNumbers;
 }
-
 const evenNumbers = arrayOfNumbers(vector);
-
 console.log(evenNumbers);
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+function countRepetitionOfElements(elementsArray) {
+  const elementsCount = {};
+  for (const element of elementsArray) {
+    if (!elementsCount[element]) {
+      elementsCount[element] = 0;
+    }
+    elementsCount[element] += 1;
+  }
+  return elementsCount;
+}
+const basketCountedObject = countRepetitionOfElements(basket)
+console.log(basketCountedObject);
+
+const moradores = {
+  blocoUm: [{
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [{
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+const nome = null;
+const sobrenome = null;
+const andar = null;
+const apartamento = null;
+const zoeyInfo = `O morador do bloco 2 de nome ${nome} ${sobrenome} mora no ${andar}° andar, apartamento ${apartamento}`;
+console.log(zoeyInfo);
