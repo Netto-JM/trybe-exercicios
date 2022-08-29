7c46e73056480497984aafbb52dea62ae02c5dbb
NettoJM <nettojm86@gmail.com>
2022-08-29 00:17:54 -0300

    finished second group of exercises

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercise1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercise1/script.js
@@ -7,16 +7,29 @@ function changeElementColor(element, color) {
   element.style.color = color;
 }
 
-function changeElementText(element, text) {
+function changeTextElement(element, text) {
   element.textContent = text;
 }
 
-function createElementWithText(element, text, className, idName) {
-  const newElement = document.createElement(element);
+function appendTextNode(element, text) {
   const textNode = document.createTextNode(text);
-  if (className) newElement.classList.add(className);
-  if (idName) setAttribute('id', idName);
-  newElement.appendChild(textNode)
+  element.appendChild(textNode);
+}
+
+function addClassesToElement(element, classArray) {
+  element.classList.add(...classArray);
+}
+
+function addIdToElement(element, idName) {
+  element.setAttribute('id', idName);
+}
+
+function createElementWithText(element, text, classArray, idName) {
+  const newElement = document.createElement(element);
+  appendTextNode(newElement, text);
+  if (classArray) addClassesToElement(newElement, classArray);
+  if (idName) addIdToElement(newElement, idName);
+  return newElement;
 }
 
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
@@ -26,4 +39,12 @@ const primeiroFilho = elementoOndeVoceEsta.previousElementSibling;
 const textNodeAtention = elementoOndeVoceEsta.nextSibling;
 const terceiroFilho = pai.lastElementChild.previousSibling.previousSibling;
 changeElementColor(pai, 'red');
-changeElementText(primeiroFilhoDoFilho, 'Eu sou o primeiroFilhoDoFilho');
+changeTextElement(primeiroFilhoDoFilho, 'Eu sou o primeiroFilhoDoFilho');
+const irmaoDoElementoOndeVoceEsta = createElementWithText('section', 'irmaoDoElementoOndeVoceEsta', undefined, 'irmaoDoElementoOndeVoceEsta');
+pai.appendChild(irmaoDoElementoOndeVoceEsta);
+const filhoDoElementoOndeVoceEsta = createElementWithText('section', 'filhoDoElementoOndeVoceEsta', undefined, 'filhoDoElementoOndeVoceEsta');
+elementoOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta);
+const filhoDoPrimeiroFilhoDoFilho = createElementWithText('section', 'filhoDoPrimeiroFilhoDoFilho', undefined, 'filhoDoPrimeiroFilhoDoFilho');
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+const FDPFDF = document.getElementById('filhoDoPrimeiroFilhoDoFilho');
+console.log(FDPFDF.parentNode.parentNode.nextElementSibling);
