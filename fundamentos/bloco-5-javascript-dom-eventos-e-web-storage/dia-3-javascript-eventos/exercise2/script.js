6afcd00f038376e82b6058ac3378628bd0cecb37
NettoJM <nettojm86@gmail.com>
2022-08-30 13:27:18 -0300

    first exercise completed

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise2/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise2/script.js
@@ -1,3 +1,38 @@
+function changeElementColor(element, color) {
+  element.style.color = color;
+}
+
+function changeTextElement(element, text) {
+  element.textContent = text;
+}
+
+function appendTextNode(element, text) {
+  const textNode = document.createTextNode(text);
+  element.appendChild(textNode);
+}
+
+function addClassesToElement(element, classArray) {
+  element.classList.add(...classArray);
+}
+
+function addIdToElement(element, idName) {
+  element.setAttribute('id', idName);
+}
+
+function createElementWithText(element, text) {
+  const newElement = document.createElement(element);
+  if (text) appendTextNode(newElement, text);
+  return newElement;
+}
+
+function completeElementBuilder(element, text, parent, classArray, idName) {
+  const newElement = createElementWithText(element, text)
+  if (classArray) addClassesToElement(newElement, classArray);
+  if (idName) addIdToElement(newElement, idName);
+  if (parent) parent.appendChild(newElement);
+  return newElement;
+}
+
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
@@ -14,3 +49,25 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+/* Crie um calendário dinamicamente.
+O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira.
+A tag <ul> deve conter o id 'days';
+Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> */
+
+const daysList = document.getElementById('days');
+
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const decemberHolidays = [24, 25, 31];
+
+const decemberFridays = [4, 11, 18, 25];
+
+for (const day of decemberDaysList) {
+  const decemberDayItem = completeElementBuilder('li', day, daysList, ['day']);
+  if (decemberHolidays.includes(day)) decemberDayItem.classList.add('holiday');
+  if (decemberFridays.includes(day)) decemberDayItem.classList.add('friday');
+}
