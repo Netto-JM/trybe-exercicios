9767d539181da5b61f502a4b804bb59b370dfd33
NettoJM <nettojm86@gmail.com>
2022-08-30 21:41:03 -0300

    seventh exercise completed

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise2/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise2/script.js
@@ -113,3 +113,10 @@ function hilghtFridays() {
 }
 
 friday.addEventListener('click', hilghtFridays);
+
+/* Implemente uma função que adicione uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".z */
+
+const myTaks = document.querySelector('.my-tasks');
+
+const customtask = completeElementBuilder('span', 'cozinhar', myTaks);
